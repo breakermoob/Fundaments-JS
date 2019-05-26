9b3ade4b76946d91c8f7596eeba61e8889f164b2
Breakermoob <breakermoob@hotmail.com>
2019-05-26 01:14:43 -0500

    First Test

---
+++ test1.js
@@ -0,0 +1,27 @@
+var hworld = 'Hello World!'
+console.log(hworld)
+
+var number
+number = 0
+console.log("the number is:" + number)
+
+number +=5
+console.log("the number is:" + number)
+
+number -=2
+console.log("the number is:" + number)
+
+number *=10
+console.log("the number is:" + number)
+
+number = number/2
+console.log("the number is:" + number)
+
+number += 0.25
+console.log("the number is:" + number)
+//
+number *= 15.25589
+console.log("the number is:" + number)
+
+//just a number of decimals is with the function .toFixed("HERE NUMBER")
+console.log("the number is:" + number.toFixed(3))  
