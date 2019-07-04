bb2b4527fbfe5ddc86719a67fbbf9dbd74fd4201
Breakermoob <breakermoob@hotmail.com>
2019-07-03 23:39:20 -0500

    Asincronismo - CallBackHell y manejo de error en network

--- Js/callbackHell.js
+++ Js/callbackHell.js
@@ -1,37 +1,59 @@
+var invocation = new XMLHttpRequest();
+var url = 'http://bar.other/resources/public-data/';
+   
+function callOtherDomain() {
+  if(invocation) {    
+    invocation.open('GET', url, true);
+    invocation.onreadystatechange = handler;
+    invocation.send(); 
+  }
+}
+
 //Api y uri para obtener la informacion
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
-
-//Parametros que recibe la funcion de JQuery para el get del API
-// const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = {crossDomain:true}
-// const response = function(persona){
-//    console.log(persona);
-// }
-//Funcion get usando JQuery
-//URL del api - cossDomain - Respuesta
-// $.get(lukeUrl,opts,response);
 
+// //Funcion con callback
+// function obtenerPersonaje(id, callback){
+//    const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+//    $.get(lukeUrl,opts,function(persona){
+//       console.log(persona.name);
+//       if (callback) {
+//          callback();
+//       }
+//    });
+// }
+
+// //En orden con callback hell
+// obtenerPersonaje(1, function(){
+//    obtenerPersonaje(2,function(){
+//       obtenerPersonaje(3,function(){
+//          obtenerPersonaje(4)
+//       })
+//    })
+// });
+
+
+//Funcion con callbackHell con manejo de errores para perdida de internet
 function obtenerPersonaje(id, callback){
    const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-   $.get(lukeUrl,opts,function(persona){
-      console.log(persona.name);
-      if (callback) {
-         callback();
-      }
+   //El fail es otro callback como arrowFunction para manejar el error
+   $.get(lukeUrl,opts,callback).fail(()=>{
+      console.log('Puta vida, error de internet :(');
    });
 }
 
-//Como son funciones asincronicas, no sabemos en que orden van a llegar
-for (let i = 1; i < 5; i++) {
-   obtenerPersonaje(i);
-}
-
 //En orden con callback hell
-obtenerPersonaje(1, function(){
-   obtenerPersonaje(2,function(){
-      obtenerPersonaje(3,function(){
-         obtenerPersonaje(4)
+obtenerPersonaje(1, function(persona){
+   console.log(persona.name);
+   obtenerPersonaje(2,function(persona){
+      console.log(persona.name);
+      obtenerPersonaje(3,function(persona){
+         console.log(persona.name);
+         obtenerPersonaje(4,function(persona){
+            console.log(persona.name)
+         })
       })
    })
 });
