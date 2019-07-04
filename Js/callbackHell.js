3a1c3ec391dd87b526a547ffafa675597dbeac33
Breakermoob <breakermoob@hotmail.com>
2019-07-03 23:16:51 -0500

    Asincronismo - CallBackHell

--- Js/callbackHell.js
+++ Js/callbackHell.js
@@ -1,15 +1,3 @@
-//Problema de CORS
-var invocation = new XMLHttpRequest();
-var url = 'http://bar.other/resources/public-data/';
-   
-function callOtherDomain() {
-  if(invocation) {    
-    invocation.open('GET', url, true);
-    invocation.onreadystatechange = handler;
-    invocation.send(); 
-  }
-}
-
 //Api y uri para obtener la informacion
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
@@ -17,18 +5,33 @@ const PEOPLE_URL = 'people/:id';
 //Parametros que recibe la funcion de JQuery para el get del API
 // const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = {crossDomain:true}
-const response = function(persona){
-   console.log(persona.name);
-}
+// const response = function(persona){
+//    console.log(persona);
+// }
 //Funcion get usando JQuery
 //URL del api - cossDomain - Respuesta
 // $.get(lukeUrl,opts,response);
 
-function obtenerPersonaje(id){
+function obtenerPersonaje(id, callback){
    const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-   $.get(lukeUrl,opts,response);
+   $.get(lukeUrl,opts,function(persona){
+      console.log(persona.name);
+      if (callback) {
+         callback();
+      }
+   });
 }
+
 //Como son funciones asincronicas, no sabemos en que orden van a llegar
-for (let index = 1; index < 4; index++) {
-   obtenerPersonaje(index);
+for (let i = 1; i < 5; i++) {
+   obtenerPersonaje(i);
 }
+
+//En orden con callback hell
+obtenerPersonaje(1, function(){
+   obtenerPersonaje(2,function(){
+      obtenerPersonaje(3,function(){
+         obtenerPersonaje(4)
+      })
+   })
+});
